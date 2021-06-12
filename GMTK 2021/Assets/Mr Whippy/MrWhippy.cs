d39d56c9a62f7ef952b76ceb105e518526f92d74
Bibliomaniac314 <73324029+Bibliomaniac314@users.noreply.github.com>
2021-06-12 13:04:54 +0930

    WhipDemon Prefabbed

--- GMTK 2021/Assets/Mr Whippy/MrWhippy.cs
+++ GMTK 2021/Assets/Mr Whippy/MrWhippy.cs
@@ -15,7 +15,10 @@ public class MrWhippy : MonoBehaviour
 
     const int SWEEP_ATTACK_HEIGHT = 3;
     const int SWEEP_ATTACK_WIDTH = 4;
-    
+
+    const int MINI_ATTACK_HEIGHT = 2;
+    const int MINI_ATTACK_WIDTH = 2;
+
     double timer = ATTACK_TIMER;
 
     List<string> attackList = new List<string>();
@@ -78,10 +81,10 @@ public class MrWhippy : MonoBehaviour
         int vertOrHor = Random.Range(0, 2);
         if (vertOrHor == 0)
             //do vert
-            attackInstance.transform.localScale = new Vector3(1, LINE_ATTACK_LENGTH, 1);
+            attackInstance.transform.localScale = new Vector3(1, LINE_ATTACK_LENGTH, 0);
         else
             //do hor
-            attackInstance.transform.localScale = new Vector3(LINE_ATTACK_LENGTH, 1, 1);
+            attackInstance.transform.localScale = new Vector3(LINE_ATTACK_LENGTH, 1, 0);
 
     }
 
@@ -90,10 +93,50 @@ public class MrWhippy : MonoBehaviour
         attackIndicator attackInstance = Instantiate(roundAttack);
 
         attackInstance.GetComponent<attackIndicator>().DefineAttack(3, 2, 1);
-        attackInstance.transform.localScale = new Vector3(SWEEP_ATTACK_WIDTH, SWEEP_ATTACK_HEIGHT, 1);
+        attackInstance.transform.localScale = new Vector3(SWEEP_ATTACK_WIDTH, SWEEP_ATTACK_HEIGHT, 0);
         attackInstance.transform.position = this.transform.position;
 
     }
+
+    void initiateTripleAttack()
+    {
+        //replace this with player movement predicting logic
+        Vector3 attackCentrePoint = new Vector3(Random.Range(0, 10), Random.Range(0, 10), 0);
+
+        Vector3 attack1 = attackCentrePoint += new Vector3(Random.Range(-3, 4), Random.Range(-3, 4), 0);
+        Vector3 attack2 = attackCentrePoint += new Vector3(Random.Range(-3, 4), Random.Range(-3, 4), 0);
+        Vector3 attack3 = attackCentrePoint += new Vector3(Random.Range(-3, 4), Random.Range(-3, 4), 0);
+
+        List<Vector3> attackList = new List<Vector3>();
+        attackList.Add(attack1);
+        attackList.Add(attack2);
+        attackList.Add(attack3);
+
+        // Prevent overlapping attacks
+        /*
+        foreach (Vector3 attack in attackList)
+        {
+            foreach (Vector3 other in attackList)
+                if (attack == other)
+                    attack
+        }
+        */
+
+        foreach (Vector3 attack in attackList)
+        {
+            int timer = 2;
+            
+            attackIndicator attackInstance = Instantiate(roundAttack);
+
+            attackInstance.GetComponent<attackIndicator>().DefineAttack(2, 2, 1);
+            attackInstance.transform.localScale = new Vector3(SWEEP_ATTACK_WIDTH, SWEEP_ATTACK_HEIGHT, 1);
+            attackInstance.transform.position = new Vector3(Random.Range(0, 10), Random.Range(0, 10), 0);
+        }
+
+
+    }
+
+
 
 }
 
