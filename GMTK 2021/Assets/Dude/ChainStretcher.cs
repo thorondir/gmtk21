fc2d17fd0a38179aa227e4477e956564213fb5bb
Ying Stokes <yingstokes@gmail.com>
2021-06-12 13:58:19 +0930

    working chain stretching, no hitboxes

---
+++ GMTK 2021/Assets/Dude/ChainStretcher.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChainStretcher : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    float length, angle;
+    public float angleOffset;
+    public GameObject ga, gb;
+    Vector3 delta;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Join(GameObject a, GameObject b) {
+        delta = a.transform.position - b.transform.position;
+        Debug.Log(delta);
+        length = delta.magnitude;
+        angle = Mathf.Rad2Deg * Mathf.Atan2(delta.y, delta.x);
+        spriteRenderer.size = new Vector2(length, 0.15f);
+        transform.eulerAngles = new Vector3(0, 0, angle + angleOffset);
+        transform.position = ((a.transform.position + b.transform.position) / 2) + new Vector3(0,0,0.8f);
+    }
+}
