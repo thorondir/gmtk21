8e82a384c2c4fad6a2c80b278c7d1fb20f8e9167
Bibliomaniac314 <73324029+Bibliomaniac314@users.noreply.github.com>
2021-06-13 22:39:15 +0930

    entryRoom beginnings

---
+++ GMTK 2021/Assets/UI/CreditsButton.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsButton : MonoBehaviour
+{
+
+    public GameObject CreditText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<Health>().myType == 0 && other == GetComponent<chainmanager>().chain[0])
+        {
+
+            showCredits();
+        
+        }
+    }
+    
+    void showCredits()
+    {
+        return;
+
+    }
+}
