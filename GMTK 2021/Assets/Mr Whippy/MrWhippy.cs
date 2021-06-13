78b6ddc5a0b7c761750f0ae9752c0a78770d54b6
Bibliomaniac314 <73324029+Bibliomaniac314@users.noreply.github.com>
2021-06-13 13:59:02 +0930

    Whip boy takes a break

--- GMTK 2021/Assets/Mr Whippy/MrWhippy.cs
+++ GMTK 2021/Assets/Mr Whippy/MrWhippy.cs
@@ -25,6 +25,10 @@ public class MrWhippy : MonoBehaviour
 
     public double ATTACK_TIMER;
     public double FAST_ATTACK_TIMER = 0.7;
+
+    public int HitsUntilBigCoolDown = 3;
+    public double BIG_COOLDOWN_LENGTH = 4;
+
     public int LINE_ATTACK_LENGTH;
 
     public int DEFEND_ATTACK_HEIGHT;
@@ -68,7 +72,6 @@ public class MrWhippy : MonoBehaviour
 
 
         this.PILLAR_BREAKING_MODE = true;
-
     }
 
     // Update is called once per frame
@@ -148,6 +151,13 @@ public class MrWhippy : MonoBehaviour
             else
                 chosenAttack = "line";
 
+        else if (this.HitsUntilBigCoolDown <= 0)
+                {
+                    //yield return new WaitForSeconds((float)BIG_COOLDOWN_LENGTH);
+        chosenAttack = "noAttack";
+        this.timer = BIG_COOLDOWN_LENGTH;
+        this.HitsUntilBigCoolDown = UnityEngine.Random.Range(3, 6);
+        }
 
         else if (this.pendingAttacks.Count == 0)
         {
@@ -158,6 +168,7 @@ public class MrWhippy : MonoBehaviour
                 int randAttack = UnityEngine.Random.Range(0, this.attackList.Count);
                 chosenAttack = this.attackList[randAttack];
             }
+            this.HitsUntilBigCoolDown -= 1;
 
         }
         else
@@ -186,8 +197,6 @@ public class MrWhippy : MonoBehaviour
         {
             this.initiateMiniAttack();
             this.timer = 1;
-
-
         }
         if (chosenAttack == "sweep")
         {
