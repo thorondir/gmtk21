a08038dd0ee43e727acd155c1f846d55e9c07213
ax <79813818+axelmhoffmann@users.noreply.github.com>
2021-06-13 19:21:07 +0930

    hello

--- GMTK 2021/Assets/SlowAnim.cs
+++ GMTK 2021/Assets/SlowAnim.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class SlowAnim : MonoBehaviour
 {
+    public ParticleSystem boom;
+    public ParticleSystem schwoop;
+    public ParticleSystem passive;
+    SpriteRenderer r;
+
+    public double timeUntilDelete=0;
     // Start is called before the first frame update
-    void Start()
+    public void Activate()
     {
-        
+        boom.Play();
+        schwoop.Play();
+        timeUntilDelete = 1;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (r == null)
+            r = GetComponent<SpriteRenderer>();
+        if (timeUntilDelete > 0)
+        {
+            Color col = r.color;
+            col.a -= 1f * Time.deltaTime;
+            r.color = col;
+            timeUntilDelete -= Time.deltaTime;
+
+            return;
+        }
+        if(timeUntilDelete < 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
